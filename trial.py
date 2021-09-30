a020577210193367cd37b5117a97ca7dcc70fdf9
Sunny  Kumar <sunny16270@iiitd.ac.in>
2021-09-30 17:50:42 +0530

    Update trial.py

--- trial.py
+++ trial.py
@@ -93,15 +93,15 @@ def hero():
         if(newperiod%2==0):
             sum=getSum(current)+check
             if(sum%2==0):
-                print(newperiod+1," : GREEN")
+                print(newperiod+1," : RED")
             else:
-                print(newperiod+1,"  : RED")
+                print(newperiod+1,"  : GREEN")
         else:
             sum=getSum(current)+check+1
             if(sum%2==0):
-                print(newperiod+1,"   : GREEN")
-            else:
                 print(newperiod+1,"   : RED")
+            else:
+                print(newperiod+1,"   : GREEN")
         newperiod+=1
         numbers.append(current)
         y=input("Do you want to play : Press 1 and 0 to exit \n")
