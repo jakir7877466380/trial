ccb44cec45e3ccd6192555ce17cbffb87f03c740
Sunny  Kumar <sunny16270@iiitd.ac.in>
2021-10-04 10:01:09 +0530

    Update trial.py

--- trial.py
+++ trial.py
@@ -185,7 +185,7 @@ else:
         bhai=input(": ")
         if(bhai==code):
             print("Your play time is 4:30,7:30 PM Today, 4th Oct 2021")
-            print("Only play on your given time:)
+            print("Only play on your given time:")
             time.sleep(20)
             period=330
             hero()
