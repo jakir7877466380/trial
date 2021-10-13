64c1613a11a778d1d4ff3d1e3e6fd9faee360267
Sunny  Kumar <sunny16270@iiitd.ac.in>
2021-10-13 12:18:20 +0530

    Update trial.py

--- trial.py
+++ trial.py
@@ -165,7 +165,7 @@ else:
         # for mac and linux(here, os.name is 'posix')
         else:
             _ = system('clear')
-    code="THALAOP21"
+    code="VIJAYSETHUPATHI"
     banner='figlet RXCE'
     system(banner)
     print("*---------*----------*-------------*----------*")
@@ -192,9 +192,9 @@ else:
         if(bhai==code):
             clear()
             print("----------Your play time-----------")
-            print("12th Oct 2021, 02:30 PM- 03:00 PM")
-            print("12th Oct 2021, 05:30 PM- 06:00 PM")
-            print("12th Oct 2021, 08:30 PM- 09:00 PM")
+            print("13th Oct 2021, 02:30 PM- 03:00 PM")
+            print("13th Oct 2021, 05:30 PM- 06:00 PM")
+            print("13th Oct 2021, 08:30 PM- 09:00 PM")
             print("Please play on the given time, and ")
             print("If you think it is an error contact")
             print("wait.... starting....")
