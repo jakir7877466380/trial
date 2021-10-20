d821caf72714302aa69a641862bf2cc1ef6b3cf8
Sunny  Kumar <sunny16270@iiitd.ac.in>
2021-10-20 13:26:12 +0530

    Update trial.py

--- trial.py
+++ trial.py
@@ -93,15 +93,15 @@ def hero():
         if(newperiod%2==0):
             sum=getSum(current)
             if(sum%2==0):
-                print(newperiod+1," :  🟢, GREEN")
+                print(newperiod+1," : 🔴, RED")
             else:
-                print(newperiod+1,"  : 🔴, RED")
+                print(newperiod+1,"  : 🟢, GREEN")
         else:
             sum=getSum(current)
             if(sum%2==0):
-                print(newperiod+1,"   :🟢, GREEN")
-            else:
                 print(newperiod+1,"   : 🔴, RED")
+            else:
+                print(newperiod+1,"   : 🟢, GREEN")
         newperiod+=1
         numbers.append(current)
         y=input("Do you want to play : Press 1 and 0 to exit \n")
@@ -192,9 +192,9 @@ else:
         if(bhai==code):
             clear()
             print("----------Your play time-----------")
-            print("19th Oct 2021, 02:30 PM- 03:00 PM")
-            print("19th Oct 2021, 05:00 PM- 05:30 PM")
-            print("19th Oct 2021, 08:30 PM- 09:00 PM")
+            print("20th Oct 2021, 02:30 PM- 03:00 PM")
+            print("20th Oct 2021, 05:00 PM- 05:30 PM")
+            print("20th Oct 2021, 08:30 PM- 09:00 PM")
             print("Please play on the given time, and ")
             print("If you think it is an error contact")
             print("wait.... starting....")
