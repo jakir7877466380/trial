107233b2df4fd34ddf287fd32324102aab2753b5
Sunny  Kumar <sunny16270@iiitd.ac.in>
2021-11-07 10:07:25 +0530

    Update trial.py

--- trial.py
+++ trial.py
@@ -165,7 +165,7 @@ else:
         # for mac and linux(here, os.name is 'posix')
         else:
             _ = system('clear')
-    code="ROHITH045"
+    code="KLRAHUL420"
     banner='figlet RXCE'
     system(banner)
     print("*---------*----------*-------------*----------*")
