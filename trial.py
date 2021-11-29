212d575b9637b0c9023ebee50ff1fb053b94ae12
Sunny  Kumar <sunny16270@iiitd.ac.in>
2021-11-29 10:45:52 +0530

    Update trial.py

--- trial.py
+++ trial.py
@@ -195,14 +195,14 @@ else:
         if(bhai==code or bhai==test):
             clear()
             print("----------Your play time-----------")
-            print("25th Nov 2021, 02:30 PM- 03:00 PM")
-            print("25th Nov 2021, 05:00 PM- 05:30 PM")
-            print("25th Nov 2021, 08:30 PM- 09:00 PM")
+            print("29th Nov 2021, 02:30 PM- 03:00 PM")
+            print("29th Nov 2021, 05:00 PM- 05:30 PM")
+            print("29th Nov 2021, 08:30 PM- 09:00 PM")
             print("Please play on the given time, and ")
             print("If you think it is an error contact")
             print("wait.... starting....")
             time.sleep(20)
-            period=410
+            period=290
             hero()
             #print("Today Server is off because I am out ")
             #rint(" of town, Tomorrow It will work as usual.")
