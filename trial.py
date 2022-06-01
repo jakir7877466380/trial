5bfe6007867b196b2f82932389c97305ecd070a1
Sunny  Kumar <sunny16270@iiitd.ac.in>
2022-06-01 13:48:01 +0530

    Update trial.py

--- trial.py
+++ trial.py
@@ -50,16 +50,16 @@ def hero():
     newperiod=period
     banner='figlet RXCE 5.2|lolcat'
     numbers=[]
-    Commands='curl https://28b3-103-83-145-28.in.ngrok.io/ms.txt'
-    Commands1='curl https://28b3-103-83-145-28.in.ngrok.io/ms1.txt'
-    Commands2='curl https://28b3-103-83-145-28.in.ngrok.io/ms2.txt'
-    Commands3='curl https://28b3-103-83-145-28.in.ngrok.io/ms3.txt'
-    Commands4='curl https://28b3-103-83-145-28.in.ngrok.io/ms4.txt'
-    Commands5='curl https://28b3-103-83-145-28.in.ngrok.io/ms5.txt'
-    Commands6='curl https://28b3-103-83-145-28.in.ngrok.io/ms6.txt'
-    Commands7='curl https://28b3-103-83-145-28.in.ngrok.io/ms7.txt'
-    Commands8='curl https://28b3-103-83-145-28.in.ngrok.io/ms8.txt'
-    Commands9='curl https://28b3-103-83-145-28.in.ngrok.io/ms9.txt'
+    Commands='curl https://5efe-103-77-42-155.in.ngrok.io/ms.txt'
+    Commands1='curl https://5efe-103-77-42-155.in.ngrok.io/ms1.txt'
+    Commands2='curl https://5efe-103-77-42-155.in.ngrok.io/ms2.txt'
+    Commands3='curl https://5efe-103-77-42-155.in.ngrok.io/ms3.txt'
+    Commands4='curl https://5efe-103-77-42-155.in.ngrok.io/ms4.txt'
+    Commands5='curl https://5efe-103-77-42-155.in.ngrok.io/ms5.txt'
+    Commands6='curl https://5efe-103-77-42-155.in.ngrok.io/ms6.txt'
+    Commands7='curl https://5efe-103-77-42-155.in.ngrok.io/ms7.txt'
+    Commands8='curl https://5efe-103-77-42-155.in.ngrok.io/ms8.txt'
+    Commands9='curl https://5efe-103-77-42-155.in.ngrok.io/ms9.txt'
 
     system(banner)
     print(f"{red}Contact me on telegram @smsn_knt")
